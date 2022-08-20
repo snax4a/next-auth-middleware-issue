f8aa8e609ec52de7950b8b98985e858e124351a3
Szymon Sus <sus.szymon3@gmail.com>
2022-08-20 14:34:47 +0200

    feat: add middleware issue example

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,38 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-// More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      // `/admin` requires admin role
-      if (req.nextUrl.pathname === "/admin") {
-        return token?.userRole === "admin"
-      }
-      // `/me` only requires the user to be logged in
-      return !!token
-    },
+/*
+ * This middleware never runs on /api/auth/signin page
+ * I would like to redirect from sign in page if user is authenticated.
+ * Same happens with custom sign in page - middleware skips it.
+ */
+export default withAuth(
+  function middleware(req) {
+    console.log("[MIDDLEWARE]: custom middleware runs...");
+
+    const hasToken = req.nextauth.token;
+    const isLoginPage = req.nextUrl.pathname === "/api/auth/signin";
+
+    if (isLoginPage && hasToken) {
+      req.nextUrl.pathname = "/me";
+      // redirect to /me page because user is already logged in
+      return NextResponse.redirect(new URL("/me", req.nextUrl));
+    }
   },
-})
+  {
+    callbacks: {
+      authorized({ req, token }) {
+        console.log("[MIDDLEWARE]: authorized callback runs...");
 
-export const config = { matcher: ["/admin", "/me"] }
+        // `/admin` requires admin role
+        if (req.nextUrl.pathname === "/admin") {
+          return token?.userRole === "admin";
+        }
+        // `/me` only requires the user to be logged in
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = { matcher: ["/admin", "/me", "/api/auth/signin"] };
